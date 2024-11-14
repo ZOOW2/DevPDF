073c1443f557c3931718cb1ce92c38e3f1d09fe5
Zoow <134532859+ZOOW2@users.noreply.github.com>
2024-11-14 16:37:47 +0300

    Первый коммит

---
+++ DevExpressTestPDF/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace DevExpressTestPDF
+{
+    partial class SearchPDF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.memoEdit2 = new DevExpress.XtraEditors.MemoEdit();
+            this.searchControl1 = new DevExpress.XtraEditors.SearchControl();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchControl1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // memoEdit2
+            // 
+            this.memoEdit2.Location = new System.Drawing.Point(256, 115);
+            this.memoEdit2.Name = "memoEdit2";
+            this.memoEdit2.Size = new System.Drawing.Size(801, 476);
+            this.memoEdit2.TabIndex = 0;
+            // 
+            // searchControl1
+            // 
+            this.searchControl1.Location = new System.Drawing.Point(398, 77);
+            this.searchControl1.Name = "searchControl1";
+            this.searchControl1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Repository.ClearButton(),
+            new DevExpress.XtraEditors.Repository.SearchButton()});
+            this.searchControl1.Size = new System.Drawing.Size(487, 20);
+            this.searchControl1.TabIndex = 1;
+            // 
+            // SearchPDF
+            // 
+            this.ClientSize = new System.Drawing.Size(1331, 634);
+            this.Controls.Add(this.searchControl1);
+            this.Controls.Add(this.memoEdit2);
+            this.Name = "SearchPDF";
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchControl1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.MemoEdit memoEdit1;
+        private DevExpress.XtraEditors.SearchControl InfoBD;
+        private DevExpress.XtraEditors.MemoEdit memoEdit2;
+        private DevExpress.XtraEditors.SearchControl searchControl1;
+    }
+}
+
---
+++ DevExpressTestPDF/Form1.cs
@@ -0,0 +1,83 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace DevExpressTestPDF
+{
+    public partial class SearchPDF : DevExpress.XtraEditors.XtraForm
+    {
+        public SearchPDF()
+        {
+            InitializeComponent();
+            searchControl1.TextChanged += SearchControl1_TextChanged;
+        }
+
+        private void SearchControl1_TextChanged(object sender, EventArgs e)
+        {
+            string fileNumber = searchControl1.Text;
+            if (!string.IsNullOrWhiteSpace(fileNumber))
+            {
+                SearchAndDisplayFile(fileNumber);
+            }
+        }
+
+        // Метод для поиска файла по номеру и вывода данных
+        private void SearchAndDisplayFile(string fileNumber)
+        {
+            string connectionString = "server=localhost;user=root;password=root;database=pdf";
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT Path FROM info WHERE ID = @ID";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ID", fileNumber);
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                string filePath = reader.GetString("Path");
+
+                                if (File.Exists(filePath))
+                                {
+                                    string fileText = ExtractTextFromPdf(filePath);
+
+                                    memoEdit2.Text = "File Content: " + Environment.NewLine + "-------------------------------------------" + Environment.NewLine;
+                                    memoEdit2.AppendText(fileText);
+                                }
+                                else
+                                {
+                                    memoEdit2.Text = "File not found.";
+                                }
+                            }
+                        }
+                        else
+                        {
+                            memoEdit2.Text = "No data found for this number.";
+                        }
+                    }
+                }
+            }
+        }
+
+        private string ExtractTextFromPdf(string filePath)
+        {
+            StringBuilder text = new StringBuilder();
+            using (iTextSharp.text.pdf.PdfReader reader = new iTextSharp.text.pdf.PdfReader(filePath))
+            {
+                for (int page = 1; page <= reader.NumberOfPages; page++)
+                {
+                    string pageText = iTextSharp.text.pdf.parser.PdfTextExtractor.GetTextFromPage(reader, page);
+
+                    text.AppendLine(pageText.Trim());
+                }
+            }
+            return text.ToString();
+        }
+    }
+}
